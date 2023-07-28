9c3ab448807a20e66fc1b6184fa4189435c88eb1
Jack Hou <jackhou147@berkeley.edu>
2023-07-27 22:22:55 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,13 @@ function App()
     return (
         <div className="App bg-grey w-full h-full flex flex-row">
             <SidePanel panelChildren={
-                <div className={"w-full h-full  flex flex-col"}>
+                <div className={"w-full h-full  flex flex-col pl-4 pr-4"}>
                     <div className={"top-bar h-12 "}>
 
                     </div>
                     <div className={"foldersContainer grow"}>
+                        <ListItem text={"New Folder"} icon={"../icons/folder_FILL0_wght400_GRAD0_opsz48.svg"}></ListItem>
                         <ListItem text={"Recently Deleted"}></ListItem>
-                        
-                        <ListItem text={"New Folder"}></ListItem>
 
 
 
--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 
-export function ListItem({text, icon, rootStyle}:{text: string, icon?: React.ReactNode, rootStyle?: Object})
+export function ListItem({text, icon, rootStyle}:{text: string, icon?: string, rootStyle?: Object})
 {
-    return <div className={"flex flex-row"} style={rootStyle}>
-        {icon} {text}
+    return <div className={"flex flex-row mb-4 bg-selectedItem-2 p-1 rounded items-center"} style={rootStyle}>
+        <div className={"w-1/6 mr-1"}> <img src={icon}/> </div> {text}
     </div>
 }
