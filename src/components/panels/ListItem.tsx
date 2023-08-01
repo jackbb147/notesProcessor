5149eb7b31f7bc3d6b2baafc0e1f41bd83223852
Jack Hou <jackhou147@berkeley.edu>
2023-08-01 00:01:47 -0700

    improvements on dark mode

--- src/components/panels/ListItem.tsx
+++ src/components/panels/ListItem.tsx
@@ -9,6 +9,6 @@ export function ListItem({text, icon, rootClassName, active, onClick}:{
 })
 {
     return <div  onClick={onClick} className={`flex flex-row ${active && "bg-selectedItem-2"} p-1 rounded items-center cursor-default  ${rootClassName} overflow-hidden`} >
-        <div className={"w-5 mr-2 min-w-[1.25rem] "}> {icon} </div> <div className={`whitespace-nowrap ${active && "font-bold"}`}>{text}</div>
+        <div className={"w-5 mr-2 min-w-[1.25rem] flex items-center "}> {icon} </div> <div className={`whitespace-nowrap ${active && "font-bold"}`}>{text}</div>
     </div>
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export function Button({icon, rootClassName, onClick}: {icon?:React.ReactNode, rootClassName?:string, onClick?:(e:React.MouseEvent)=>any})
 {
-    return <div  onClick={onClick} className={"ml-1 cursor-default "+rootClassName}>{icon}</div>
+    return <div  onClick={onClick} className={"flex items-center  ml-1 cursor-default "+rootClassName}>{icon}</div>
 }
