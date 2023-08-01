13c941f193a79cc292f145ce819a2b55e2d63940
Jack Hou <jackhou147@berkeley.edu>
2023-08-01 03:43:11 -0700

    works great

--- src/components/editor/NoteEditor.tsx
+++ src/components/editor/NoteEditor.tsx
@@ -69,11 +69,31 @@ function QuillBoxComponent({val, handleBlur, onFinishSetup, onTouchStart, isRead
     function setUpQuill(container:HTMLElement)
     {
 
+        const toolbarOptions:any  = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
+
         var quillNode = new Quill(container, {
 
             theme: "snow",
             modules:{
-                toolbar:true,
+                toolbar:toolbarOptions,
                 MathEditorModule: {},
                 keyboard: {
                     bindings: MathEditorModule.getBindings()
