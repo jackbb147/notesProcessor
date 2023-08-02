57c859ca8e744567cc82540a640313b669e23a3b
Jack Hou <jackhou147@berkeley.edu>
2023-08-02 01:10:07 -0700

    working on label selector colors

--- src/components/editor/LabelSelector.tsx
+++ src/components/editor/LabelSelector.tsx
@@ -2,6 +2,7 @@ import {useGraph, useGraphDispatch, useState} from "../../reducers/hooks";
 import CreatableSelect from "react-select/creatable";
 import Select from "react-select/base";
 import {stat} from "fs";
+import {CSSObjectWithLabel} from "react-select";
 export function LabelSelector()
 {
     const graph = useGraph();
@@ -16,12 +17,39 @@ export function LabelSelector()
     ]
 
     return (
-        <div className={"text-black"}>
+        <div style={{color: "black"}}>
             <CreatableSelect
-                classNames={{
-                    // control: (state)=>{
-                    //     return "bg"
-                    // }
+
+                styles={{
+                    control: (base, state):CSSObjectWithLabel=>({
+                        ...base,
+                        backgroundColor: "transparent"
+                    }),
+                    menu: (base, state):CSSObjectWithLabel=>({
+                        ...base,
+                        backgroundColor: appState.darkModeOn ? state.theme.colors.neutral80 : "transparent"
+
+                    }),
+
+                    menuList: (base, state):CSSObjectWithLabel=>({
+                        ...base,
+                        // color: appState.darkModeOn ?
+
+                    }),
+
+                    option: (base, state)=>({
+                        ...base,
+                        backgroundColor: appState.darkModeOn ? (
+                            state.isFocused ? state.theme.colors.neutral0 : "transparent"
+                        ) : (
+                            state.isFocused ? state.theme.colors.primary50 : "transparent"
+                        ),
+                        color: appState.darkModeOn ? (
+                            state.isFocused ? "black" : "white"
+                        ) : (
+                            state.isFocused ? "black" : "black"
+                        )
+                    }),
                 }}
                 menuPlacement={"top"}
                 isMulti
