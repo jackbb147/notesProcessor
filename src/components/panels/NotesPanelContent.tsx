31f4da8542040dedd767f1fbcd6d8f52ef029d04
Jack Hou <jackhou147@berkeley.edu>
2023-08-02 19:47:28 -0700

    transition works! nice.

--- src/components/panels/NotesPanelContent.tsx
+++ src/components/panels/NotesPanelContent.tsx
@@ -18,7 +18,7 @@ function AnimatedList({ data}:{data:Node[]}) {
     const transitions = useTransition(data, {
         from: { opacity: 0, transform: "scale(0)", },
         enter: { opacity: 1, transform: "scale(1)",},
-        leave: { opacity: 0, transform: "scale(0)" },
+        leave: { opacity: 0, transform: "scale(0)", height:"0" },
         keys: item => item.id
     })
 
