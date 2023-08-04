3ca33c61dbf5a0b2e5870f1ea4b6c33d5852fd1c
Jack Hou <jackhou147@berkeley.edu>
2023-08-04 01:13:10 -0700

    working on upload feature

--- src/components/panels/Buttons/UploadButton.tsx
+++ src/components/panels/Buttons/UploadButton.tsx
@@ -8,43 +8,7 @@ export function UploadButton()
     const state =useAppState()
     const dispatch = useDispatch()
     const ref = useRef<any>(null)
-    const inputRef = useRef<any>(null)
-
-
-
-    function handleClick()
-    {
-        if(!inputRef || !inputRef.current)
-        {
-            console.error("input not ready.")
-            return;
-        }
-        inputRef.current.click();
-    }
-
-    function handleInputChange(this: any)
-    {
-        let that:React.ReactNode = this;
-        if(!inputRef ||  !inputRef.current || !inputRef.current.files)
-        {
-            console.error("input ref not ready, or no file property. ")
-        }
-
-        const reader = new FileReader();
-        reader.onload = (e)=>{
-
-            if (typeof reader.result !== "string") {
-                alert(" There's something wrong with the file you uploaded :( ")
-                return;
-            }
-
-            const obj = JSON.parse(reader.result);
-            debugger;
-
-        }
-
-        reader.readAsText(inputRef.current.files[0]);
-    }
+    const upload = useUpload()
 
     return (
             <>
@@ -52,17 +16,11 @@ export function UploadButton()
                     ref={ref}
                     text={"Upload Notes"}
                     iconOnly={state.LabelPanelClosed}
-                    onClick={handleClick}
+                    onClick={upload}
                     icon={<span className="material-symbols-outlined">
-upload
-</span>}
+                        upload
+                    </span>}
                 ></ListItem>
-                <input style={{
-                    display: "none"
-                }}
-                       ref={inputRef}
-                       onChange={handleInputChange}
-                       type={"file"}/>
             </>
 
     )
--- src/useUpload.tsx
+++ src/useUpload.tsx
@@ -5,8 +5,33 @@ async function read(input: HTMLElement)
     //     TODO
 
     input.click();
+
 }
 
+function handleInputChange(this: any)
+{
+
+    if(!this || !this.files)
+    {
+        console.error("input element corrupted. ")
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e)=>{
+
+        if (typeof reader.result !== "string") {
+            alert(" There's something wrong with the file you uploaded :( ")
+            return;
+        }
+
+        const obj = JSON.parse(reader.result);
+        debugger;
+
+    }
+
+    reader.readAsText(this.files[0]);
+}
 
 
 
@@ -15,8 +40,13 @@ export function useUpload()
     const input = document.createElement("input");
     input.type = "file";
     input.style.display = "none";
+    input.onchange = handleInputChange;
     document.body.append(input);
     return ()=>{
         read(input)
     }
 }
+
+
+
+
