4b23f8ac3a45d645bf54b458932ae13a6f3880f9
Jack Hou <jackhou147@berkeley.edu>
2023-08-03 19:01:06 -0700

    working on side panel (looking good!)

--- src/components/panels/Buttons/SettingsButton.tsx
+++ src/components/panels/Buttons/SettingsButton.tsx
@@ -1,15 +1,24 @@
 import {useDispatch, useState} from "../../../reducers/hooks";
 import {ListItem} from "../ListItem";
 import React from "react";
+import {AppActionType} from "../../../reducers/AppStateReducer";
 
 export function SettingsButton()
 {
     const state =useState()
     const dispatch = useDispatch()
 
+
+    function handleClick()
+    {
+        dispatch({
+            type: AppActionType.setShowSettingsPanel,
+            show: !state.showSettingsPanel
+        })
+    }
     return (
         <ListItem text={`Settings`}
-                  // onClick={handleDarkModeTogglerClick}
+                  onClick={handleClick}
                   iconOnly={state.LabelPanelClosed}
                   icon={<span className="material-symbols-outlined">
                         settings
