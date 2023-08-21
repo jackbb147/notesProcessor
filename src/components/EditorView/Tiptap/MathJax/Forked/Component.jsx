1ad0fcb906f60321e663c457014782a1c97204e3
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-21 13:54:44 -0700

    successfully added mathjax autocompleter back in

--- src/components/EditorView/Tiptap/MathJax/Forked/Component.jsx
+++ src/components/EditorView/Tiptap/MathJax/Forked/Component.jsx
@@ -1,12 +1,15 @@
 import { NodeViewWrapper } from "@jackhou147/tiptap/packages/react";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-latex";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { MATHJAXCOMMANDS } from "../../../Quill/quillModules/Quill-MathJax/mathjaxCommands";
 
 export default (props) => {
+  const reactAceRef = useRef(null);
+  const [completerConfigured, setCompleterConfigured] = React.useState(false);
   const increase = () => {
     props.updateAttributes({
       count: props.node.attrs.count + 1,
@@ -17,6 +20,32 @@ export default (props) => {
     console.log("change", newValue);
   }
 
+  useEffect(() => {
+    if (!reactAceRef.current) return;
+    if (completerConfigured) return;
+    const editor = reactAceRef.current.editor;
+    // debugger;
+    var staticWordCompleter = {
+      getCompletions: function (editor, session, pos, prefix, callback) {
+        // var wordList = [String.raw `\foo`, "bar", "baz"];
+        var wordList = MATHJAXCOMMANDS;
+        callback(
+          null,
+          wordList.map(function (word) {
+            return {
+              caption: word,
+              value: word,
+              meta: "static",
+            };
+          }),
+        );
+      },
+    };
+
+    editor.completers.push(staticWordCompleter);
+    setCompleterConfigured(true);
+  }, [reactAceRef.current]);
+
   return (
     <NodeViewWrapper className="react-component">
       <span className="label">React Component</span>
@@ -26,6 +55,7 @@ export default (props) => {
           This button has been clicked {props.node.attrs.count} times.
         </button>
         <AceEditor
+          ref={reactAceRef}
           mode="latex"
           theme="monokai"
           enableLiveAutocompletion={true}
