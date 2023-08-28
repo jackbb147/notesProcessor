2d14a2d3e51d6efe0ef6ca0e1cdd12011a1f8f9a
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-27 22:20:32 -0700

    some documentation

--- src/components/EditorView/Tiptap/Reference/ReferenceMapContext.tsx
+++ src/components/EditorView/Tiptap/Reference/ReferenceMapContext.tsx
@@ -6,6 +6,7 @@ import React, {
   useState,
 } from "react";
 
+export type ReferenceMap = Map<string, number>;
 export const ReferenceMapContext = createContext<Map<string, number>>(
   new Map(),
 );
@@ -14,12 +15,17 @@ export const SetReferenceMapContext = createContext<
   Dispatch<SetStateAction<Map<any, any>>>
 >(null as unknown as Dispatch<SetStateAction<Map<any, any>>>);
 
+/**
+ * This component provides a context for the reference map.
+ * @param children
+ * @constructor
+ */
 export function ReferenceMapProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [referenceMap, setReferenceMap] = useState(new Map());
+  const [referenceMap, setReferenceMap] = useState<ReferenceMap>(new Map());
 
   return (
     <ReferenceMapContext.Provider value={referenceMap}>
