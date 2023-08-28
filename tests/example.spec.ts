1eacb2ccf8aafd6fc9871db29d3689ef3e7ba80c
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-28 01:24:16 -0700

    add some tests in playwright! all pass

---
+++ tests/example.spec.ts
@@ -0,0 +1,97 @@
+import { test, expect } from "@playwright/test";
+
+test("test add reference", async ({ page }) => {
+  await page.goto("http://localhost:3000/notesProcessor");
+  await page.getByText("test 3").click();
+  await page.getByText("Hello from test3!").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3!$/ })
+    .nth(4)
+    .fill("Hello from test3! [[");
+  await page.getByRole("button", { name: "test2" }).click();
+  await page.getByText("test2").nth(2).click();
+  await page.getByText("test 3").nth(1).click();
+});
+
+test("test add then delete reference", async ({ page }) => {
+  await page.goto("http://localhost:3000/notesProcessor");
+  await page.getByText("test 3").click();
+  await page.getByText("Hello from test3!").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3!$/ })
+    .nth(4)
+    .fill("Hello from test3! [[");
+  await page.getByRole("button", { name: "test1" }).click();
+  await page.getByText("test1").nth(2).click();
+  await page.getByText("test 3").nth(1).click();
+  await page.getByText("Hello from test3! test1").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3! test1$/ })
+    .nth(4)
+    .fill("Hello from test3! ");
+  await page.getByText("article test1").first().click();
+});
+
+test("test add two references", async ({ page }) => {
+  await page.goto("http://localhost:3000/notesProcessor");
+  await page.getByText("test 3").click();
+  await page.getByText("Hello from test3!").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3!$/ })
+    .nth(4)
+    .fill("Hello from test3! [[");
+  await page.getByRole("button", { name: "test2" }).click();
+  await page.getByText("Hello from test3! test2").click();
+  await page.locator(".tiptap").fill("Hello from test3! test2   [[");
+  await page.getByRole("button", { name: "test2" }).click();
+  await page.getByText("article").nth(3).click();
+  await page.getByText("test 3").nth(1).click();
+});
+
+test("test referencedBy", async ({ page }) => {
+  await page.goto("http://localhost:3000/notesProcessor");
+  await page.getByText("article test 3").click();
+  await page.getByText("Hello from test3!").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3!$/ })
+    .nth(4)
+    .fill("Hello from test3! [[");
+  await page.getByRole("button", { name: "test2" }).click();
+  await page.getByText("test2").nth(2).click();
+  await expect(page.getByText("test 4").nth(1)).not.toBeVisible();
+  await expect(page.getByText("test 3").nth(1)).toBeVisible();
+});
+
+test("test add one reference", async ({ page }) => {
+  await page.goto("http://localhost:3000/notesProcessor");
+  await page.getByText("test 3").click();
+  await page.getByText("Hello from test3!").click();
+  await page
+    .locator("div")
+    .filter({ hasText: /^Hello from test3!$/ })
+    .nth(4)
+    .fill("Hello from test3! [[");
+  await page.getByRole("button", { name: "test1" }).click();
+  await expect(page.getByText("test1").nth(2)).toBeVisible();
+});
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+//
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
