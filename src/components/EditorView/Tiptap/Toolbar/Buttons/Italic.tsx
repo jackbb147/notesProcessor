6809681c1109081bbe320f6dcf0de5daddb04856
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-28 19:09:42 -0700

    italic button works too

--- src/components/EditorView/Tiptap/Toolbar/Buttons/Italic.tsx
+++ src/components/EditorView/Tiptap/Toolbar/Buttons/Italic.tsx
@@ -1,11 +1,29 @@
 import { MenuItem } from "./MenuItem";
 import { FontItalicIcon } from "@radix-ui/react-icons";
 import { Editor } from "@tiptap/core";
+import { useEffect, useState } from "react";
 
 export function Italic({ editor }: { editor: Editor | null }) {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick() {
+    if (!editor) return;
+    editor.chain().focus().toggleItalic().run();
+  }
+
+  useEffect(() => {
+    editor?.on("selectionUpdate", ({ editor }) => {
+      if (editor.isActive("italic")) {
+        console.debug("[italic] is active");
+        setIsActive(true);
+      } else {
+        setIsActive(false);
+      }
+    });
+  }, [editor]);
   return (
-    <MenuItem value={"italic"} ariaLabel={"Italic"} isActive={false}>
-      <FontItalicIcon />
+    <MenuItem value={"italic"} ariaLabel={"Italic"} isActive={isActive}>
+      <FontItalicIcon onClick={handleClick} />
     </MenuItem>
   );
 }
