6dd8cf58643e21748069b3454fd7318ded5142da
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-28 23:15:04 -0700

    Working on select

--- src/components/EditorView/Tiptap/Toolbar/Buttons/Select.tsx
+++ src/components/EditorView/Tiptap/Toolbar/Buttons/Select.tsx
@@ -25,7 +25,7 @@ const SelectItem = React.forwardRef(
       <Select.Item
         value={value}
         className={classnames(
-          "text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1",
+          "text-[13px] leading-none  rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1",
           className,
         )}
         {...props}
@@ -43,18 +43,18 @@ const SelectItem = React.forwardRef(
 const SelectDemo = () => (
   <Select.Root>
     <Select.Trigger
-      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
+      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px]   hover:bg-grey hover:text-violet11 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none"
       aria-label="Food"
     >
       <Select.Value placeholder="Select a fruit…" />
-      <Select.Icon className="text-violet11">
+      <Select.Icon>
         <ChevronDownIcon />
       </Select.Icon>
     </Select.Trigger>
 
     <Select.Portal>
-      <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+      <Select.Content className="overflow-hidden  bg-dark_primary rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white cursor-default">
           <ChevronUpIcon />
         </Select.ScrollUpButton>
         <Select.Viewport className="p-[5px]">
