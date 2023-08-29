3b3538316f64a84dcfa8034dfc3c564fe5f65cd5
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-28 23:09:49 -0700

    Working on select

--- src/components/EditorView/Tiptap/Toolbar/Toolbar.tsx
+++ src/components/EditorView/Tiptap/Toolbar/Toolbar.tsx
@@ -9,6 +9,7 @@ import {
   FontItalicIcon,
 } from "@radix-ui/react-icons";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import * as Select from "@radix-ui/react-select";
 
 import { MenuItem } from "./Buttons/MenuItem";
 import { Editor } from "@tiptap/core";
@@ -20,6 +21,7 @@ import { Redo } from "./Buttons/Redo";
 import { LeftAlign } from "./Buttons/LeftAlign";
 import { Center } from "./Buttons/Center";
 import { RightAlign } from "./Buttons/RightAlign";
+import SelectDemo from "./Buttons/Select";
 
 export const MyToolbar = ({ editor }: { editor: Editor | null }) => (
   <Toolbar.Root
@@ -37,16 +39,11 @@ export const MyToolbar = ({ editor }: { editor: Editor | null }) => (
       <Underline editor={editor} />
     </Toolbar.ToggleGroup>
     <Toolbar.Separator className="w-[1px] bg-mauve6 mx-[10px]" />
+    <SelectDemo />
+    <Toolbar.Separator className="w-[1px] bg-mauve6 mx-[10px]" />
     <Toolbar.ToggleGroup type="single" aria-label="Text alignment">
       <LeftAlign editor={editor} />
       <Center editor={editor} />
-      {/*<Toolbar.ToggleItem*/}
-      {/*  className="flex-shrink-0 flex-grow-0 basis-auto  h-[25px] px-[5px] rounded inline-flex text-[13px] leading-none items-center justify-center  ml-0.5 outline-none hover:bg-grey hover:text-violet11 focus:relative focus:shadow-[0_0_0_2px] focus:shadow-violet7 first:ml-0 data-[state=on]:bg-violet5 data-[state=on]:text-violet11"*/}
-      {/*  value="center"*/}
-      {/*  aria-label="Center aligned"*/}
-      {/*>*/}
-      {/*  <TextAlignCenterIcon />*/}
-      {/*</Toolbar.ToggleItem>*/}
       <RightAlign editor={editor} />
     </Toolbar.ToggleGroup>
     <Toolbar.Separator className="w-[1px] bg-mauve6 mx-[10px]" />
