d5f006bec517fe78368e37f628c21a53f1d9aebc
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-28 23:24:03 -0700

    Working on select

--- src/components/EditorView/Tiptap/Toolbar/Buttons/Select.tsx
+++ src/components/EditorView/Tiptap/Toolbar/Buttons/Select.tsx
@@ -25,7 +25,7 @@ const SelectItem = React.forwardRef(
       <Select.Item
         value={value}
         className={classnames(
-          "text-[13px] leading-none  rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1",
+          "text-[13px]  text-white leading-none  rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-white data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white",
           className,
         )}
         {...props}
@@ -43,7 +43,7 @@ const SelectItem = React.forwardRef(
 const SelectDemo = () => (
   <Select.Root>
     <Select.Trigger
-      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px]   hover:bg-grey hover:text-violet11 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none"
+      className="inline-flex items-center justify-center rounded px-[5px] text-[13px] leading-none  hover:bg-grey hover:text-violet11 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none"
       aria-label="Food"
     >
       <Select.Value placeholder="Select a fruit…" />
@@ -53,13 +53,17 @@ const SelectDemo = () => (
     </Select.Trigger>
 
     <Select.Portal>
-      <Select.Content className="overflow-hidden  bg-dark_primary rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white cursor-default">
+      <Select.Content
+        position={"popper"}
+        side={"bottom"}
+        className="overflow-hidden  bg-dark_primary rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
+      >
+        <Select.ScrollUpButton className="flex items-center justify-center h-[15px] bg-white cursor-default">
           <ChevronUpIcon />
         </Select.ScrollUpButton>
-        <Select.Viewport className="p-[5px]">
+        <Select.Viewport className="">
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label className="px-[15px] text-xs leading-[25px] text-mauve11">
               Fruits
             </Select.Label>
             <SelectItem value="apple">Apple</SelectItem>
