8175c9e1851bc61781524841291cdc849ec35be0
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-08-31 16:18:05 -0700

    successfully did a migration from V0 to V1

--- src/_migration/GraphStates/V0.ts
+++ src/_migration/GraphStates/V0.ts
@@ -1,14 +1,34 @@
-export interface GraphNode {
-  id: string;
-  title: string;
-  labels: string[];
-  dateLastModified?: string;
-  dateCreated?: string;
-  content: string;
-}
+import { z } from "zod";
+// export interface GraphNode {
+//   id: string;
+//   title: string;
+//   labels: string[];
+//   dateLastModified?: string;
+//   dateCreated?: string;
+//   content: string;
+// }
 
-export interface V0 {
-  nodes: GraphNode[];
-  labels: string[];
-  deletedNodes: GraphNode[];
-}
+export const GraphNode = z.object({
+  id: z.string(),
+  title: z.string(),
+  labels: z.array(z.string()),
+  dateLastModified: z.string().optional(),
+  dateCreated: z.string().optional(),
+  content: z.string(),
+});
+
+export type GraphNode = z.infer<typeof GraphNode>;
+
+// export interface V0 {
+//   nodes: GraphNode[];
+//   labels: string[];
+//   deletedNodes: GraphNode[];
+// }
+
+export const V0 = z.object({
+  nodes: z.array(GraphNode),
+  labels: z.array(z.string()),
+  deletedNodes: z.array(GraphNode),
+});
+
+export type V0 = z.infer<typeof V0>;
--- src/components/Buttons/UploadButton.tsx
+++ src/components/Buttons/UploadButton.tsx
@@ -7,6 +7,8 @@ import { ListItem } from "./ListItem";
 import React, { useRef } from "react";
 import { useUpload } from "../../hooks/useUpload";
 import { GraphActionType, GraphState } from "../../reducers/GraphReducer";
+import { migrate } from "../../_migration/GraphStateMigration";
+import { Version } from "../../Version";
 
 export function UploadButton() {
   const state = useAppState();
@@ -16,11 +18,22 @@ export function UploadButton() {
   const upload = useUpload();
 
   async function handleClick() {
-    let obj = (await upload()) as GraphState;
-    graphDispatch({
-      type: GraphActionType.merge, // todo maybe change this to "set" because "merge" creates trouble when the versions aren't the same??  Or maybe migrate first, before merging?
-      other: obj,
-    });
+    let obj = await upload();
+    try {
+      const migratedObj = migrate(obj, Version.GraphState);
+      let parseResult = GraphState.safeParse(migratedObj);
+      if (!parseResult.success) {
+        console.error(parseResult.error);
+        return;
+      }
+      graphDispatch({
+        type: GraphActionType.set, // todo maybe change this to "set" because "merge" creates trouble when the versions aren't the same??  Or maybe migrate first, before merging?
+        state: parseResult.data,
+      });
+    } catch (e) {
+      console.error("[uploadButton] error migrating uploaded graph:  ", e);
+      return;
+    }
   }
 
   return (
