e95c46a07a5c9353feb97beadf2d49bb33f32f10
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-02 13:34:19 -0700

    bug fix. Good to deploy

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,8 +13,7 @@ export function Button({
 }) {
   return (
     <div
-      onMouseDown={onClick}
-      onTouchStart={onClick}
+      onClick={onClick}
       className={
         " overflow-hidden flex items-center  ml-1 cursor-default cursor-pointer " +
         rootClassName
