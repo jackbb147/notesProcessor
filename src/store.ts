560cedb7e05a52c9447763b26dccf7fd667c1de8
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-10 16:32:08 -0700

    gradually replacing locally stored notes with cloud notes instead

---
+++ src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+// Or from '@reduxjs/toolkit/query/react'
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { apiSlice } from "./api/apiSlice";
+
+export const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware: any) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
