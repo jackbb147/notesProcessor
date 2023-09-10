1dfdc9b6f6c523d476051e53eabdd2f6160805e7
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-10 14:07:09 -0700

    added a hook to join signalR room

---
+++ src/hooks/useRoom.tsx
@@ -0,0 +1,64 @@
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from "@microsoft/signalr";
+import { useEffect, useState } from "react";
+
+/**
+ * This hook is used to join a SignalR room.
+ * @param userName The username.
+ * @param roomName The room name. Note that users with the same room name will be in the same room.
+ * If room name is the same as user name, then a user logged in on multiple devices will be in the same room.
+ */
+export function useRoom({
+  userName,
+  roomName,
+}: {
+  userName: string;
+  roomName: string;
+}) {
+  const [connection, setConnection] = useState<HubConnection>();
+  const [updateNeeded, setUpdateNeeded] = useState<number>(0);
+  const joinRoom = async (user: string, room: string) => {
+    try {
+      const connection = new HubConnectionBuilder()
+        .withUrl("/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      connection.on("ReceiveMessage", (user, message) => {
+        // alert(`Message received: ${message}`);
+        console.log(`Message received: ${message}`);
+      });
+
+      connection.on("DeletedNote", (user, noteId) => {
+        // alert(`Deleted note: ${noteId}`);
+        console.log(`Deleted note: ${noteId}`);
+        // apiSlice.endpoints.getNotes.invalidateTags(["notes"]);
+        setUpdateNeeded((updateNeeded) => updateNeeded + 1);
+      });
+
+      connection.on("RecoveredNote", (user, noteId) => {
+        // alert(`Recovered note: ${noteId}`);
+        console.log(`Recovered note: ${noteId}`);
+        // apiSlice.endpoints.getNotes.invalidateTags(["notes"]);
+        setUpdateNeeded((updateNeeded) => updateNeeded + 1);
+      });
+
+      await connection.start();
+      await connection.invoke("JoinRoom", { user, room });
+      setConnection(connection);
+    } catch (e) {
+      alert(JSON.stringify(e, null, 2));
+    }
+  };
+
+  useEffect(() => {
+    if (userName && roomName) {
+      joinRoom(userName, roomName);
+    }
+  }, [userName, roomName]);
+
+  return [connection, updateNeeded];
+}
