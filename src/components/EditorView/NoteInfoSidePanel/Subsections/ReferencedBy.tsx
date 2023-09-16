685f9858c6b4a7357f27d8d9c601022b8817b5d9
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-15 20:12:22 -0700

    small cleanup

--- src/components/EditorView/NoteInfoSidePanel/Subsections/ReferencedBy.tsx
+++ src/components/EditorView/NoteInfoSidePanel/Subsections/ReferencedBy.tsx
@@ -12,47 +12,19 @@ import { NoteItem } from "../NoteItem";
 export function ReferencedBy({ note }: { note: GraphNode }) {
   const { data: notes } = useGetNotesQuery();
   const { data: links } = useGetLinksQuery();
-  const [graphology, updated] = useGraphology();
-  const GraphState = useGraph();
-  const [InNeighbors, setInNeighbors] = useState<string[]>([]);
-
-  // useEffect(() => {
-  //   if (!notes) {
-  //     console.warn("[ReferencedBy] no notes available");
-  //     return;
-  //   }
-  //   if (!links) {
-  //     console.warn("[ReferencedBy] no links available");
-  //     return;
-  //   }
-  //   try {
-  //     console.log("LinksToThisNote");
-  //     // if (!graphology.hasNode(note.Id)) {
-  //     //   console.warn("[ReferencedBy] Node not found in graphology: " + note.Id);
-  //     //   console.debug(
-  //     //     "[ReferencedBy] graphology nodes: " +
-  //     //       JSON.stringify(graphology.nodes(), null, 2),
-  //     //   );
-  //     // } else {
-  //     //   setInNeighbors((v) => graphology.inNeighbors(note.Id));
-  //     // }
-  //     // console.log("InNeighbors", InNeighbors);
-  //     var inNeighbors = ;
-  //
-  //     setInNeighbors(inNeighbors);
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }, [updated, note]);
-
   if (!notes) return null;
   if (!links) return null;
   const inNeighborIds = Array.from(
     new Set(
       links
         .filter((link) => link.TargetId === note.Id && !link.Deleted)
-        .map((link) => link.SourceId),
+        .map((link) => link.SourceId)
+        .filter((id) => notes.some((note) => note.Id === id)),
     ),
+  );
+
+  const inNeighbors: GraphNode[] = inNeighborIds.map(
+    (id) => notes.find((note) => note.Id === id)!,
   );
   return (
     <>
@@ -61,15 +33,9 @@ export function ReferencedBy({ note }: { note: GraphNode }) {
       >
         <Title text={"Referenced By"} />
         <div>
-          {/*  THIS IS TO AVOID DUPLICATES */}
-          {inNeighborIds.map((id) => {
-            const node = notes.find((node) => node.Id === id);
-            if (node) {
-              return <NoteItem note={node} />;
-            } else {
-              return null;
-            }
-          })}
+          {inNeighbors.map((node) => (
+            <NoteItem note={node} />
+          ))}
         </div>
       </div>
       <Separator />
