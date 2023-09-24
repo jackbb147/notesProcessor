b3616b6866a939446de26c490c22763897d49fc5
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-24 16:03:04 -0700

    working on user settings panel

--- src/components/Buttons/UserButton.tsx
+++ src/components/Buttons/UserButton.tsx
@@ -1,18 +1,32 @@
 import { ListItem } from "./ListItem";
-import { useAppState, useUser } from "../../hooks/AppStateAndGraphAndUserhooks";
+import {
+  useAppDispatch,
+  useAppState,
+} from "../../hooks/AppStateAndGraphAndUserhooks";
 import { useLogInStatus } from "../../hooks/useLogInStatus";
+import { AppActionType } from "../../reducers/AppStateReducer";
 
 export function UserButton() {
   // const activeUser = useUser();
+  const AppDispatch = useAppDispatch();
   const [loginStatus, userName] = useLogInStatus();
   const AppState = useAppState();
   if (!userName) return null;
+
+  function handleClick() {
+    // alert("clicked");
+    AppDispatch({
+      type: AppActionType.setShowUserSettingsPanel,
+      show: !AppState.showUserSettingsPanel,
+    });
+  }
 
   return (
     <ListItem
       text={userName}
       icon={<span className="material-symbols-outlined">account_circle</span>}
       iconOnly={AppState.LabelPanelClosed}
+      onClick={handleClick}
     />
   );
 }
