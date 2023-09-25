2f28a140dd7e40a827b240401d81309edb7978d1
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-09-24 21:34:17 -0700

    working on upload button

--- src/components/Buttons/UploadButton.tsx
+++ src/components/Buttons/UploadButton.tsx
@@ -6,10 +6,15 @@ import {
 import { ListItem } from "./ListItem";
 import React, { useRef } from "react";
 import { useUpload } from "../../hooks/useUpload";
-import { GraphActionType, GraphState } from "../../reducers/GraphReducer";
+import {
+  GraphActionType,
+  GraphNode,
+  GraphState,
+} from "../../reducers/GraphReducer";
 import { migrate } from "../../_migration/GraphStateMigration";
 import { Version1_0 } from "../../_migration/GraphStates/V1_0";
 import { Version } from "../../Version";
+import { useAddNoteMutation } from "../../api/apiSlice";
 
 export function UploadButton() {
   const state = useAppState();
@@ -17,6 +22,14 @@ export function UploadButton() {
   const graphDispatch = useGraphDispatch();
   const ref = useRef<any>(null);
   const upload = useUpload();
+  const [
+    addNote,
+    {
+      isLoading: addNoteIsLoading,
+      isError: addNoteIsError,
+      error: addNoteError,
+    },
+  ] = useAddNoteMutation();
 
   async function handleClick() {
     let obj = await upload();
@@ -31,6 +44,31 @@ export function UploadButton() {
       }
       debugger;
       // TODO
+      const migratedGraphState: Version1_0 = parseResult.data;
+      for (const node of migratedGraphState.nodes) {
+        const nodeToUpload: GraphNode = {
+          Id: node.id,
+          Content: node.content,
+          Title: node.title,
+          DateCreated: node.dateCreated,
+          DateLastModified: node.dateLastModified,
+          Deleted: false,
+        };
+        await addNote(nodeToUpload);
+      }
+
+      for (const node of migratedGraphState.deletedNodes) {
+        const nodeToUpload: GraphNode = {
+          Id: node.id,
+          Content: node.content,
+          Title: node.title,
+          DateCreated: node.dateCreated,
+          DateLastModified: node.dateLastModified,
+          Deleted: true,
+        };
+        await addNote(nodeToUpload);
+      }
+
       // upload all the notes, labels, links to the server.
       // graphDispatch({
       //   type: GraphActionType.set, // todo maybe change this to "set" because "merge" creates trouble when the versions aren't the same??  Or maybe migrate first, before merging?
