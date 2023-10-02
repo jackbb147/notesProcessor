e0e43adae057feed2120fb2b35c79e83b767e868
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-10-01 18:15:24 -0700

    working on a button to add [[

---
+++ src/components/EditorView/Tiptap/Toolbar/Buttons/AddReferenceBtn.tsx
@@ -0,0 +1,40 @@
+import { Bold, RailSymbol } from "lucide-react";
+import { useEffect, useState } from "react";
+import { MenuItem } from "./MenuItem";
+import { IconSize } from "./IconSize";
+import { Editor } from "@tiptap/core";
+
+export function AddReferenceBtn({ editor }: { editor: Editor | null }) {
+  const [isActive, setIsActive] = useState(false);
+  function handleClick() {
+    if (!editor) return;
+    const selection = editor.state.selection;
+    const currentPos = selection.$anchor.pos; // this is the current position of the cursor
+    console.log("selection", selection);
+    editor.chain().focus().insertContent("[[").run();
+    editor.chain().focus().run();
+    // editor.chain().focus().toggleBold().run();
+  }
+
+  // useEffect(() => {
+  //     editor?.on("selectionUpdate", ({ editor }) => {
+  //         if (editor.isActive("bold")) {
+  //             console.debug("[bold] is active");
+  //             setIsActive(true);
+  //         } else {
+  //             setIsActive(false);
+  //         }
+  //     });
+  // }, [editor]);
+
+  return (
+    <MenuItem
+      value={"bold"}
+      ariaLabel={"Bold"}
+      isActive={isActive}
+      onClick={handleClick}
+    >
+      <RailSymbol size={IconSize} />
+    </MenuItem>
+  );
+}
