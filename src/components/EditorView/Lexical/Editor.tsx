f4579628735cee2208068f3e777e7feee5da069a
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-10-05 15:31:52 -0700

    working on the inline math editor plugin

--- src/components/EditorView/Lexical/Editor.tsx
+++ src/components/EditorView/Lexical/Editor.tsx
@@ -4,15 +4,52 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { useEffect } from "react";
-import { $getRoot, $insertNodes, EditorState } from "lexical";
+import {
+  $getRoot,
+  $insertNodes,
+  COMMAND_PRIORITY_EDITOR,
+  createCommand,
+  EditorState,
+  LexicalCommand,
+} from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { UpdateHandlerPlugin } from "./UpdateHandlerPlugin";
 import { GraphNode } from "../../../reducers/GraphReducer";
 
-import { InlineMathNode } from "./Math/InlineMathNode";
+import { $createInlineMathNode, InlineMathNode } from "./Math/InlineMathNode";
 import { HTMLToLexicalPlugin } from "./HTMLToLexicalPlugin";
+import { $insertNodeToNearestRoot } from "@lexical/utils";
+export const INSERT_INLINE_MATH_COMMAND: LexicalCommand<string> =
+  createCommand();
+
+export function InlineMathPlugin() {
+  const [editor] = useLexicalComposerContext();
+  function handleButtonPress() {
+    // alert("Button Pressed!");
+    editor.dispatchCommand(INSERT_INLINE_MATH_COMMAND, "test");
+  }
+  useEffect(() => {
+    if (!editor.hasNodes([InlineMathNode])) {
+      throw new Error(
+        "[InlineMathPlugin]: InlineMathNode not registered on editor (initialConfig.nodes)",
+      );
+    }
+
+    return editor.registerCommand<string>(
+      INSERT_INLINE_MATH_COMMAND,
+      (payload) => {
+        const inlineMathNode = $createInlineMathNode(payload);
+        $insertNodeToNearestRoot(inlineMathNode);
+
+        return true;
+      },
+      COMMAND_PRIORITY_EDITOR,
+    );
+  }, [editor]);
+  return <button onClick={handleButtonPress}>Press Me</button>;
+}
 
 export function Editor({ note }: { note: GraphNode }) {
   return (
@@ -35,6 +72,7 @@ export function Editor({ note }: { note: GraphNode }) {
         <UpdateHandlerPlugin />
         <HTMLToLexicalPlugin html={note.Content} />
         <HistoryPlugin />
+        <InlineMathPlugin />
       </div>
     </LexicalComposer>
   );
