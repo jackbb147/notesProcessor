21ca21000f9d0ddc91be7546755ab214ebfd7188
Jack Hou <54428926+jackbb147@users.noreply.github.com>
2023-10-12 09:32:24 -0700

    working on implementing a keystroke combination for easy insertion of math block

--- src/components/EditorView/Lexical/Math/InlineMathPlugin.tsx
+++ src/components/EditorView/Lexical/Math/InlineMathPlugin.tsx
@@ -1,16 +1,41 @@
 import { useEffect } from "react";
 import { LexicalEditor, TextNode } from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { $createInlineMathNode, InlineMathNode } from "./InlineMathNode";
+
+function textNodeTransform(node: TextNode): void {
+  let targetNode: InlineMathNode | null = findAndTransformInlineMath(node);
+}
+
+function findAndTransformInlineMath(node: TextNode): InlineMathNode | null {
+  const textContent = node.getTextContent();
+
+  for (var i = 0; i < textContent.length; i++) {
+    const chunk = textContent.slice(i, i + 3);
+    if (/\$.\$/.test(chunk)) {
+      // debugger;
+      let targetNode;
+      console.log("found inline math: " + chunk);
+      if (i == 0) {
+        [targetNode] = node.splitText(i + 3);
+      } else {
+        [, targetNode] = node.splitText(i, i + 3);
+      }
+      // debugger;
+      console.log("targetNode: " + targetNode.getTextContent());
+      const inlineMathNode = $createInlineMathNode("hello world");
+      targetNode.replace(inlineMathNode);
+      return inlineMathNode;
+    }
+  }
+
+  return null;
+}
 
 function useInlineMath(editor: LexicalEditor) {
   useEffect(() => {
     const removeTransform = editor.registerNodeTransform(TextNode, (node) => {
-      // console.log("hello: " + node.getTextContent());
-      const textContent = node.getTextContent();
-      if (/\$.\$/.test(textContent)) {
-        //     TODO
-        console.log("found inline math");
-      }
+      textNodeTransform(node);
     });
     return () => {
       removeTransform();
